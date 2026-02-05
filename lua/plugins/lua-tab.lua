876a424e252adb53aec328d484f782f5ffb791b7
Noriyaki <Noriyaki@avdul.com>
2026-02-05 18:38:24 +0100

    Added lua tab to the plugins list

---
+++ lua/plugins/lua-tab.lua
@@ -0,0 +1,15 @@
+return{
+    { 
+	'alvarosevilla95/luatab.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	config = function()
+	    require('luatab').setup{
+		-- title = function() return '' end,
+		-- modified = function() return '' end,
+		-- windowCount = function() return '' end,
+		-- devicon = function() return '' end,
+		-- separator = function() return '' end,
+	    }
+	end, 
+    },
+}
