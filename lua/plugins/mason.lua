7a7248876857ba5427ff98665e964b749b11c8ea
Ab <Ab@desktop.com>
2026-03-01 21:32:29 +0000

    WIP

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -37,12 +37,12 @@ return {
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
 	opts = {
 	    ensure_installed = {
-		"prettier", -- prettier formatter
+		-- "prettier", -- prettier formatter
 		"stylua", -- lua formatter
 		"isort", -- python formatter
 		"black", -- python formatter
 		"pylint",
-		"eslint_d",
+		-- "eslint_d",
 	    },
 	},
 	dependencies = {
